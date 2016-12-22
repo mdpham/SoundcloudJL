49223e01f4b1b653ec7f501a180ca8882bc6f63b
Martin Pham <mdpham@uwaterloo.ca>
2016-12-21 20:26:48 -0500

    print statements and organizing

--- scrape.jl
+++ scrape.jl
@@ -20,7 +20,8 @@ end
 function getlinkedpartition(res::Dict, collection::Array)
   nextcollection = res["collection"]
   push!(collection, nextcollection...)
-  if haskey(res, "next_href")
+  # if haskey(res, "next_href")
+  if false
     println("...")
     req = Requests.get(res["next_href"])
     res = Requests.json(req)
@@ -52,7 +53,7 @@ end
 
 # Return an array of track objects representing all favorites by user
 function userfavorites(userid::Integer)
-  println("Getting user favourites")
+  # println("Getting user favourites")
   req = Requests.get(baseapi*"/users/"*string(userid)*"/favorites", query = Dict("client_id" => clientid, "linked_partitioning" => "1"))
   res = Requests.json(req)
   favorites = getlinkedpartition(res, [])
@@ -61,7 +62,7 @@ end
 
 # Return an array of playlist objects representing all playlists by user
 function userplaylists(userid::Integer)
-  println("Getting user playlists")
+  # println("Getting user playlists")
   req = Requests.get(baseapi*"/users/"*string(userid)*"/playlists", query = Dict("client_id" => clientid, "linked_partitioning" => "1"))
   res = Requests.json(req)
   playlists = getlinkedpartition(res, [])
@@ -71,27 +72,30 @@ end
 function sortplaylists(playlists::Array, format::Regex)
   weeklyplaylists = filter(pl -> ismatch(format, pl["title"]), playlists)
   duration = mapreduce(pl -> pl["duration"], +, 0, weeklyplaylists)
-  println("Total playlists: $(length(weeklyplaylists))")
-  println("Total number of hours: $(duration/1000/60/60)")
+  # println("Total playlists: $(length(weeklyplaylists))")
+  # println("Total number of hours: $(duration/1000/60/60)")
   return weeklyplaylists
 end
 
 # PUT IT ALL TOGETHER
 function scrapefavorites(userid::Integer)
   favorites = userfavorites(userid)
-  downloadtracks(favorites)
+  cd(() -> downloadtracks(favorites), "favorites")
 end
 function scrapeplaylists(userid::Integer, format::Regex=r"")
   playlists = userplaylists(userid)
   if isequal(userid, 49699208)
-    println("Downloading phamartin ($(userid)) formatted weekly playlists")
+    # println("Downloading phamartin ($(userid)) formatted weekly playlists")
     format = r"\[[0-9]{2}\.[0-9]{2}\.[0-9]{2}\]"
   end
   weeklyplaylists = sortplaylists(playlists, format)
-  for p in weeklyplaylists
+  cd("playlists")
+  for playlist in weeklyplaylists
     title = playlist["title"]
     playlisttracks = playlist["tracks"]
-    println("Downloading $(length(playlisttracks)) artwork from $(title)")
+    # println("Downloading $(length(playlisttracks)) artwork from $(title)")
+    # cd(() -> downloadtracks(playlisttracks), "playlists")
     downloadtracks(playlisttracks)
   end
+  cd("..")
 end
--- start.jl
+++ start.jl
@@ -2,15 +2,21 @@ include("scrape.jl")
 include("collage.jl")
 
 cwd = pwd()
+
+
+
 config = getconfig()
 xdim = config["xdim"]
 ydim = config["ydim"]
 imgwidth = 100
 userurl = config["userurl"]
+
+@printf("Starting Soundcloud artwork collage:\n")
+@printf("\t%s\n", userurl)
+
 userid = resolveuser(userurl)
 
-@printf("Building Soundcloud artwork collage:\n")
-@printf("\t%s\n...Resolved userid: %i\n", userurl, userid)
+@printf("\t...resolved userid: %i\n", userid)
 @printf("\t%i wide %i tall collage with tile size %s\n", xdim, ydim, imgwidth)
 
 try
@@ -22,9 +28,13 @@ end
 cd("temp")
 
 if config["favorites"]
+  @printf("Building favorites collage\n")
   mkdir("favorites")
-  cd(() -> scrapefavorites(userid), "favorites")
+  @printf("\tDownloading artwork\n")
+  scrapefavorites(userid)
   favoritesimages = gettempartwork("favorites")
+  @printf("\t...total downloaded: %i\n", length(favoritesimages))
+  @printf("\tConcatenating images into collage\n")
   collage = init_collage(xdim, ydim, imgwidth)
   fill_collage(collage, favoritesimages)
   output = Image(collage.data')
@@ -32,9 +42,12 @@ if config["favorites"]
   cd("..")
 end
 if config["playlists"]
+  @printf("Building playlists collage\n")
   mkdir("playlists")
-  cd(() -> scrapeplaylists(userid), "playlists")
+  @printf("\tDownloading artwork\n")
+  scrapeplaylists(userid)
   playlistsimages = gettempartwork("playlists")
+  @printf("\t...total downloaded: %i\n", length(playlistsimages))
   collage = init_collage(xdim, ydim, imgwidth)
   fill_collage(collage, playlistsimages) # Don't need to assign to collage variable...
   output = Image(collage.data')
